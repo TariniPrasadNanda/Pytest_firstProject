f65710a11ae8d8bce38d7c3f9f798d1641f64a16
TariniPrasadNanda <tarininanda1996@gmail.com>
2023-08-19 14:52:03 +0530

    git commit -m "done"

--- qq.py
+++ qq.py
@@ -1,12 +1,27 @@
-s="my name is chiku"
-# count=0
-# for x in s:
-#     if x=="i":
-#         count+=1
-#         if count==2:
-#             x="n"
-#
-#     print(x,end="")
-d=s.split()
-for i in d:
-    print(i[::-1],end=" ")
+# s="my name is chiku"
+# # count=0
+# # for x in s:
+# #     if x=="i":
+# #         count+=1
+# #         if count==2:
+# #             x="n"
+# #
+# #     print(x,end="")
+# d=s.split()
+# for i in d:
+#     print(i[::-1],end=" ")
+
+import random as r
+
+ph_no = []
+
+
+ph_no.append(r.randint(6, 9))
+
+
+for i in range(1, 10):
+    ph_no.append(r.randint(0, 9))
+
+
+for i in ph_no:
+    print(i, end="")
